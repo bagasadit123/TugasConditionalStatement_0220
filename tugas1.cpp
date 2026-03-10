0a7107390f8c7d554d266aeed1f512961f8af5b7
bagasadit123 <bagasadit430@gmail.com>
2026-03-10 13:04:35 +0700

    banner main

--- tugas1.cpp
+++ tugas1.cpp
@@ -26,3 +26,10 @@ string statusBMI(float bmi) {
         return "Obesitas";
     }
 }
+
+int main() {
+    float berat, tinggi, bmi;
+
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+}
+  
